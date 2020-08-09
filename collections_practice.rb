c5388c1eb65286cb574d4dca59f3130da51de655
Venice Vargas <venicevargas1@live.com>
2020-08-09 18:40:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,11 @@ def sort_array_desc(array)
 end
 
 sort_array_desc([25, 7, 14])
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+sort_array_char_count(["dogs", "cat", "Horses"])
